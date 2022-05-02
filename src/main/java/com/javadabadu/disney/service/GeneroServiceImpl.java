2aad4f72fd1e7b5e066c89fc72d2799e7f518b92
Facundo Cordoba <fscordoba@gylgroup.com>
2022-05-02 13:21:37 -0300

    se modifica la estructura de los metodos del controlador

--- src/main/java/com/javadabadu/disney/service/GeneroServiceImpl.java
+++ src/main/java/com/javadabadu/disney/service/GeneroServiceImpl.java
@@ -3,7 +3,9 @@ package com.javadabadu.disney.service;
 import com.javadabadu.disney.exception.ExceptionBBDD;
 import com.javadabadu.disney.models.entity.Genero;
 import com.javadabadu.disney.repository.GeneroRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Locale;
 
+@Slf4j
 @Service
 public class GeneroServiceImpl implements GeneroService {
 
@@ -20,8 +23,10 @@ public class GeneroServiceImpl implements GeneroService {
     @Autowired
     private MessageSource message;
 
+
     @Override
     @Transactional(readOnly = true)
+    @CachePut(value = "findGeneroById")
     public Genero findById(Integer id) throws ExceptionBBDD {
         return generoRepository.findById(id).orElseThrow(()-> new ExceptionBBDD(message.getMessage("id.not.found", new String[]{Integer.toString(id)}, Locale.US)));
     }
@@ -34,8 +39,12 @@ public class GeneroServiceImpl implements GeneroService {
 
     @Override
     @Transactional
-    public Genero save(Genero genero , Integer id) throws ExceptionBBDD {
-        return responseBBDD(generoRepository.create(id, genero.getNombre(),genero.getImagen()),id);
+    public synchronized Genero save(Genero genero, Integer id) throws ExceptionBBDD {
+        if (existsById(id)) {
+            return responseBBDD(generoRepository.update(id, genero.getNombre(), genero.getImagen()), id);
+        } else {
+            return responseBBDD(generoRepository.create(id, genero.getNombre(), genero.getImagen()), id);
+        }
     }
 
     @Override
@@ -55,11 +64,7 @@ public class GeneroServiceImpl implements GeneroService {
     @Override
     @Transactional
     public String softDelete(Integer id) {
-        if (generoRepository.softDelete(id)) {
-            return "Se elimino el genero seleccionado";
-        }
-        return "Error en la transaccion contacte con su ADMI";
-
+        return (generoRepository.softDelete(id)) ? "Se elimino el genero seleccionado" : "Error en la transaccion contacte con su ADMI";
     }
 
     @Override
@@ -68,13 +73,13 @@ public class GeneroServiceImpl implements GeneroService {
     }
 
     @Override
-    public Genero responseBBDD(String response, Integer id) throws ExceptionBBDD{
+    public Genero responseBBDD(String response, Integer id) throws ExceptionBBDD {
         responseBBDDOk(response);
-        System.out.println(findById(id));
-        System.out.println(" <<<<<<<<<<<<<<<<<<<<<<<REPOSITORI!!!!!!!!!!!! ");
-        System.out.println(generoRepository.findById(id).get());
-        return generoRepository.findById(id).get();
+        log.info(response);
+        log.info("find By Id :" + id);
+        return findById(id);
     }
+
     private void responseBBDDOk(String response) throws ExceptionBBDD {
         if (!response.contains("Se creo correctamente") && !response.contains("Se modifico correctamente")) {
             throw new ExceptionBBDD(response);
