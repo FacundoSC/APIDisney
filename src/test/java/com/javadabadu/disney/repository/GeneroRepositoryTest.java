b1f5a79907ba7a8ca0aa29729864d10322b1e89c
Facundo Cordoba <fscordoba@gylgroup.com>
2022-05-16 13:33:26 -0300

    se suben todos los  test de pruebas unitarias , del repositorio de genero

--- src/test/java/com/javadabadu/disney/repository/GeneroRepositoryTest.java
+++ src/test/java/com/javadabadu/disney/repository/GeneroRepositoryTest.java
@@ -1,69 +1,128 @@
 package com.javadabadu.disney.repository;
 
-import com.javadabadu.disney.exception.ExceptionBBDD;
 import com.javadabadu.disney.models.entity.Genero;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.MessageSource;
-import org.springframework.http.HttpStatus;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.Locale;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class GeneroRepositoryTest {
     @Autowired
     private GeneroRepository generoRepository;
-    @Autowired
-    private MessageSource message;
+    private Integer id;
+    private Optional<String> tag;
+
+    @BeforeAll
+    static void initAll() {
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+    }
+
+    @BeforeEach
+    void init(TestInfo testInfo) {
+        tag = testInfo.getTags().stream().findFirst();
+        if (!tag.isEmpty()) {
+            id = generoRepository.lastValueId();
+            if (tag.get().equals("LastId"))
+                id -= 1;
+        }
+    }
 
     @DisplayName("Validar referencia no nula generoRepository")
     @Test
+    @Order(1)
     void generoRepositoryNotNullTest() {
         assertNotNull(generoRepository, "la referencia al  repositorio genero es null");
     }
 
+    @DisplayName("Validar obtencion del proximo id de genero")
     @Test
-    void lastValueIdTest() {
-        int esperado = generoRepository.lastValueId(); //otra forma
-        int actual = generoRepository.lastValueId();
-        assertEquals(esperado, actual, "los identificadores no son iguales");
+    @Order(2)
+    @Tag("NextID")
+    void NextIdNotNullTest() {
+        assertTrue(id instanceof Integer, "no es posible obtener el ultimo identificador");
     }
+
+    @DisplayName("Validar borrado del un genero")
     @Test
-    @Disabled
+    @Order(3)
+    @Tag("LastId")
     void softDeleteTest() {
-        assertEquals(true, generoRepository.softDelete(1), "fallo al intentar deshabilitar genero");
+        assertEquals(true, generoRepository.softDelete(id), "fallo al intentar deshabilitar genero");
     }
 
+    @DisplayName("Validar la existencia de un genero")
     @Test
-    void existGeneroByIdTest() {
-        boolean actual = generoRepository.existsById(1);
-        assertTrue(actual, "no existe genero con ese identificador");
+    @Order(4)
+    @Tag("LastId")
+    void ExistGeneroByIdTest() {
+        assertTrue(generoRepository.existsById(id), "no existe genero con ese identificador");
     }
 
+    @DisplayName("Validar la no existencia de un genero")
     @Test
-    void noExistGeneroByIdTest() {
-        assertFalse(generoRepository.existsById(-1), "existe genero con ese identificador");
+    @Order(5)
+    @Tag("NextID")
+    void NotExistGeneroByIdTest() {
+        assertTrue(generoRepository.existsById(id), "existe genero con ese identificador");
     }
 
+    @DisplayName("Validar el encontrar genero  por Id")
     @Test
-    void getGeneroForIdTest() {
-        assertTrue(generoRepository.findById(1).get() instanceof Genero);
+    @Order(6)
+    @Tag("LastId")
+    void FindGeneroForIdTest() {
+        assertTrue(generoRepository.findById(id).get() instanceof Genero);
     }
 
+    @DisplayName("Validar el no encontrar genero  por Id")
     @Test
-    void NotGetGeneroForIdTest() {
-        Integer id = -1;
-        ExceptionBBDD thrown = assertThrows(ExceptionBBDD.class, () -> {
-            generoRepository.findById(id).orElseThrow(() -> new ExceptionBBDD(message.getMessage("id.not.found", new String[]{Integer.toString(id)}, Locale.US), HttpStatus.NOT_FOUND));
-        });
-        assertEquals(message.getMessage("id.not.found", new String[]{Integer.toString(id)}, Locale.US), thrown.getMessage(), "El genero se ha encontrado");
+    @Order(7)
+    @Tag("NextID")
+    void NotFindGeneroForIdTest() {
+        Optional<Genero> genero = generoRepository.findById(id);
+        assertTrue(genero.isPresent(), " no se encontro el genero buscado por id");
     }
+
+    @DisplayName("Validar el obtener todos los generos")
+    @Test
+    @Order(8)
+    void getAllGeneroTest() {
+        List<Genero> generos = generoRepository.findAll();
+        assertFalse(generos.isEmpty(), "no se encuentran elementos  en la  lista");
+    }
+
+    @DisplayName("Validar el guardar un genero")
+    @Test
+    @Order(9)
+    @Tag("NextID")
+    void SaveGeneroTest() {
+        Genero genero = new Genero();
+        genero.setId(id);
+        genero.setAlta(true);
+        genero.setNombre("fdgdgdg");
+        genero.setImagen("img");
+        genero.setAudioVisuals(null);
+        Genero actual = generoRepository.save(genero);
+        assertTrue(actual instanceof Genero, "no se pudo crear la nueva entidad");
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
 }
+
+
+
+
